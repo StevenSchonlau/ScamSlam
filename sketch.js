213ee111c43ae332999dde1938a1958954f243a5
StevenSchonlau <sschonla@purdue.edu>
2022-09-30 21:44:07 -0400

    Start and level select menus created

--- sketch.js
+++ sketch.js
@@ -6,8 +6,6 @@ let w;
 let h;
 let border;
 
-let btnStart;
-
 let screen = 0;
 let screenDrawn = 1;
 
@@ -17,6 +15,7 @@ function updateContainer() {
   container = select('#sketchContainer');
   w = parseFloat(getComputedStyle(container.elt).getPropertyValue('width'));
   h = parseFloat(getComputedStyle(container.elt).getPropertyValue('height'));
+  
 }
 
 function windowResized() {
@@ -26,81 +25,103 @@ function windowResized() {
 
 function setup() {
   updateContainer();
-  canvas = createCanvas(w, h, WEBGL);
+  canvas = createCanvas(w, h);
   smooth();
   canvas.parent("#sketchContainer");
-  btnStart = createButton('Start');
-  btnStart.position(w/2, h*1.25);
-  btnStart.size(w/2, h/8);
-  btnStart.mousePressed(startGame);
-  btnStart.hide();
+
 }
 
 function setupScreen(screenNum) {
   if(screenNum == 0) {
+    //translate(-width / 2, -height / 2);
+// background    
     background('#fffff8');
-    btnStart.show();
+
+// border
+    stroke('#222831');
+    noFill();
+    strokeWeight(1);
+    rectMode("corners");
+    rect(0, 0, w, h);
+//button
+    strokeWeight(3);
+    rectMode("corners");
+    fill('#dddddd');
+    rect(w/4, h/2, w*(3/4), h*(5/8));
+//text
+    fill('#010101');
+    textFont("monospace");
+    strokeWeight(1);
+    textSize(height/9);
+    textAlign("center", "center");
+    text('Start', w/2, h*(9/16));
+    screenDrawn = 0;
+    //text
+    fill('#010101');
+    textFont("monospace");
+    strokeWeight(1);
+    textSize(height/9);
+    textAlign("center", "center");
+    text('Scam Slam', w/2, h*(3/16));
     screenDrawn = 0;
   }
   if(screenNum == 1){
-    clearScreen();
     background('#fffff8');
+    // border
+    stroke('#222831');
+    noFill();
+    strokeWeight(1);
+    rectMode("corners");
+    rect(0, 0, w, h);
     screenDrawn = 1;
+    //levels
+    const levels = [];
+    for(i = 0; i < 4; i++){
+      levelBtnWidth = w*(1/4)*i + 70;
+      for(j = 0; j < 2; j++){
+        levelBtnHeight = h*(1/3)*j + 150;
+        strokeWeight(3);
+        rectMode("center");
+        if(i+1+(4*(j)) == 1){ //need to have check which levels are unlocked, also add k for different menu (k = 12*menu)
+          fill('#228822');
+        } else {
+          fill('#dddddd');
+        }
+        rect(levelBtnWidth, levelBtnHeight, 100, 100);
+        fill('#010101');
+        textFont("monospace");
+        strokeWeight(1);
+        textSize(height/9);
+        textAlign("center", "center");
+        text(String(i+1+(4*j)), levelBtnWidth, levelBtnHeight);
+      }
+    }
+    //text
+    fill('#010101');
+    textFont("monospace");
+    strokeWeight(1);
+    textSize(height/9);
+    textAlign("center", "center");
+    text('Levels', w/2, h*(3/16));
+    screenDrawn = 0;
   }
 }
 
-function clearScreen(){
-  btnStart.hide();
-}
-
-function startGame(){
-  screen = 1;
+function mousePressed(){
+  if(screen == 0) {
+    if(mouseX > w/4 && mouseX < w*(3/4) && mouseY > h/2 && mouseY < h*(5/8)){
+      screen = 1;
+    }
+  }
 }
 
 function draw() {
   if (screen != screenDrawn){
     setupScreen(screen);
   }
-  /*
-  translate(-width / 2, -height / 2);
-
-  background('#fffff8');
-
-  strokeWeight(1);
-  stroke('#222831');
-  fill('#222831');
-
-  stroke('#c7b198');
-  fill('#c7b198');
-  ellipse(mouseX, mouseY, 50, 50);
-
-  // drawings
-  push();
-  stroke('#c7b198');
-  strokeWeight(2);
-  translate(width / 2, height / 2);
-  noFill();
-  rotateY(angle);
-  rotateX(angle);
-  box(100, 100);
-
-  stroke('#321f28');
-  fill('#321f28');
-  // noFill();
-  sphere(25);
-
-  pop();
-
-  // border
-  stroke('#222831');
-  noFill();
-  strokeWeight(5);
-  rectMode("corners");
-  rect(0, 0, width, height);
-  angle += 0.01;
-
-  document.getElementById("fps").innerHTML = frameRate().toFixed(2);
-  */
+  
+  //document.getElementById("fps").innerHTML = frameRate().toFixed(2);
+  
 }
 
 function colorAlpha(aColor, alpha) {
@@ -112,3 +133,5 @@ function colorAlpha(aColor, alpha) {
   }
   return color('rgba(${[red(c), green(c), blue(c), a].join(', ')})');
 }
+
+
