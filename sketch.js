98abd3aced383fc21a0c80ab676e589de161f68a
StevenSchonlau <sschonla@purdue.edu>
2022-10-07 16:29:21 -0400

    levelOneScreenStarted

--- sketch.js
+++ sketch.js
@@ -11,6 +11,15 @@ let screenDrawn = 1;
 
 let angle = 0;
 
+let characterX = 150;
+let characterY = 150;
+
+let imgCharRest1;
+let imgCharRest2;
+//import { setupStartScreen } from "./start_screen";
+//import CharacterRest1, CharacterRest2 from "./assets";
+
+
 function updateContainer() {
   container = select('#sketchContainer');
   w = parseFloat(getComputedStyle(container.elt).getPropertyValue('width'));
@@ -29,13 +38,17 @@ function setup() {
   smooth();
   canvas.parent("#sketchContainer");
 
+  imgCharRest1 = loadImage('assets/CharacterRest1.png'); //load rest 1
+  image(imgCharRest1, 10, 10);
+  imgCharRest2 = loadImage('assets/CharacterRest2.png'); //load rest 2
+  image(imgCharRest2, 10, 10);
 }
 
 function setupScreen(screenNum) {
-  if(screenNum == 0) {
+  if(screenNum == 0) { //start screen
     //translate(-width / 2, -height / 2);
 // background    
-    background('#fffff8');
+background('#fffff8');
 
 // border
     stroke('#222831');
@@ -65,7 +78,7 @@ function setupScreen(screenNum) {
     text('Scam Slam', w/2, h*(3/16));
     screenDrawn = 0;
   }
-  if(screenNum == 1){
+  if(screenNum == 1){ //level select screen
     background('#fffff8');
     // border
     stroke('#222831');
@@ -74,7 +87,8 @@ function setupScreen(screenNum) {
     rectMode("corners");
     rect(0, 0, w, h);
     screenDrawn = 1;
-    //levels
+
+    //level buttons
     const levels = [];
     for(i = 0; i < 4; i++){
       levelBtnWidth = w*(1/4)*i + 70;
@@ -96,22 +110,77 @@ function setupScreen(screenNum) {
         text(String(i+1+(4*j)), levelBtnWidth, levelBtnHeight);
       }
     }
-    //text
+
+    //back button
+    fill('#dddddd');
+    rectMode("corners");
+    strokeWeight(3);
+    rect(10, 15, 90, 65);
+    textFont("monospace");
+    fill('#010101');
+    strokeWeight(1);
+    textSize(height/12);
+    text('Back', 50, 40);
+
+    //title text
     fill('#010101');
     textFont("monospace");
     strokeWeight(1);
     textSize(height/9);
     textAlign("center", "center");
     text('Levels', w/2, h*(3/16));
-    screenDrawn = 0;
+    
+    screenDrawn = 1;
+  }
+  if(screenNum == 2){ //level 1
+    background('#fffff8');
+
+    stroke('#222831');
+    noFill();
+    strokeWeight(1);
+    rectMode("corners");
+    rect(0, 0, w, h);
+    rectMode("center");
+    image(imgCharRest1, characterX, characterY, w/15, h/9);
+    /*
+    if(((new Date.getTime()) % 100000) > 50000) { //trying to get time to do different things for rest
+      image(imgCharRest1, w/2, h/2, w/15, h/9); //character
+    } else {
+      image(imgCharRest2, w/2, h/2, w/15, h/9);
+    }
+    */
+    screenDrawn = 2;
   }
 }
 
 function mousePressed(){
-  if(screen == 0) {
+  if(screen == 0) { //Start screen buttons
+    //start button
     if(mouseX > w/4 && mouseX < w*(3/4) && mouseY > h/2 && mouseY < h*(5/8)){
       screen = 1;
     }
+  }
+  if(screen == 1) { //level select buttons
+    if(mouseX > 10 && mouseX < 90 && mouseY > 15 && mouseY < 65){ //back button
+      screen = 0;
+    }
+    for(i = 0; i < 4; i++){ // level buttons
+      levelBtnWidth = w*(1/4)*i + 70; 
+      for(j = 0; j < 2; j++){
+        levelBtnHeight = h*(1/3)*j + 150;
+        if(mouseX > levelBtnWidth-50 && mouseX < levelBtnWidth + 50 && mouseY > levelBtnHeight-50 && mouseY < levelBtnHeight+50){
+          screen = i+2+(4*(j));  //level screens start at 2
+        }    
+      }
+    }
+  }
+}
+
+function keyPressed() {
+  if(keyCode === "LEFT_ARROW") {
+    characterX -= 1;
+  } else if (keyCode === "RIGHT_ARROW") {
+    characterX += 1;
   }
 }
 
@@ -119,7 +188,9 @@ function draw() {
   if (screen != screenDrawn){
     setupScreen(screen);
   }
-  
+  if(screen == 2) {
+    setupScreen(screen);
+  }
   //document.getElementById("fps").innerHTML = frameRate().toFixed(2);
   
 }
---
+++ start_screen.js
@@ -0,0 +1,34 @@
+import { canvas, container } from "./sketch";
+
+export function setupStartScreen () {
+    //translate(-width / 2, -height / 2);
+// background    
+    background('#fffff8');
+
+// border
+    stroke('#222831');
+    noFill();
+    strokeWeight(1);
+    rectMode("corners");
+    rect(0, 0, w, h);
+//button
+    strokeWeight(3);
+    rectMode("corners");
+    fill('#dddddd');
+    rect(w/4, h/2, w*(3/4), h*(5/8));
+//text
+    fill('#010101');
+    textFont("monospace");
+    strokeWeight(1);
+    textSize(height/9);
+    textAlign("center", "center");
+    text('Start', w/2, h*(9/16));
+    screenDrawn = 0;
+    //text
+    fill('#010101');
+    textFont("monospace");
+    strokeWeight(1);
+    textSize(height/9);
+    textAlign("center", "center");
+    text('Scam Slam', w/2, h*(3/16));
+}
